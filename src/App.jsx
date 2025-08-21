6482385f93afdfdabbb02d96b25dcd3676275649
Muhammad Arfa <arfaravian@gmail.com>
2025-08-21 19:56:00 +0500

    setup all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-
-
-
+import About from './Components/About'
+import Contact from './Components/Contact'
+import Education  from './Components/Education'
+import Experience  from './Components/Experience'
+import Footer from './Components/Footer'
+import NavBar from './Components/NavBar'
+import Skills from './Components/Skills'
+import Work from './Components/Work'
 
 
 function App() {
   return (
     <div >
-
+    
     </div>
   );
 }
